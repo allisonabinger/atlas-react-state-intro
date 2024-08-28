5624b18faa5838f4f6f0d656b5302e192c223a28
Allison <allisonabinger@gmail.com>
2024-08-28 15:42:25 -0500

    A - 08/28 03:40 PM - filtered data is functional, sorted courses is functional, added up and down arrows next to header for asc and desc, header box sizes changes on click

--- src/SchoolCatalog.jsx
+++ src/SchoolCatalog.jsx
@@ -2,36 +2,127 @@ import { useEffect, useState } from "react";
 
 function SchoolCatalog() {
   const [courses, setCourses] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [sortColumn, setSortColumn] = useState("trimester");
+  const [sortDirection, setSortDirection] = useState("asc");
 
   useEffect(() => {
     fetch("../api/courses.json")
       .then((response) => response.json())
       .then((courses) => setCourses(courses));
+  }, []);
+
+  // update searchTerm state
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // updates sortDirection state
+  const handleSort = (column) => {
+    if (sortColumn === column) {
+      setSortDirection(sortDirection === "asc" ? "desc" : "asc");
+    } else {
+      setSortColumn(column);
+      setSortDirection("asc");
+    }
+  };
+
+  // filters courses (case insensitive) using searchTerm
+  const filteredCourses = courses.filter((course) => {
+    const courseNumberMatch = course.courseNumber
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const courseNameMatch = course.courseName
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    return courseNumberMatch || courseNameMatch;
+  });
+
+  // sorts courses based on value in header clicked
+  const sortedCourses = [...filteredCourses].sort((a, b) => {
+    let comparison = 0;
+
+    if (sortColumn === "trimester") {
+      comparison = Number(a.trimester) - Number(b.trimester);
+    }
+    if (sortColumn === "courseNumber") {
+      comparison = a.courseNumber.localeCompare(b.courseNumber);
+    }
+    if (sortColumn === "courseName") {
+      comparison = a.courseName.localeCompare(b.courseName);
+    }
+    if (sortColumn === "semesterCredits") {
+      comparison = Number(a.semesterCredits) - Number(b.semesterCredits);
+    }
+    if (sortColumn === "totalClockHours") {
+      comparison = Number(a.totalClockHours) - Number(b.totalClockHours);
+    }
+    return sortDirection === 'asc' ? comparison : -comparison
   });
 
   return (
     <div className="school-catalog">
       <h1>School Catalog</h1>
-      <input type="text" placeholder="Search" />
+      <input
+        type="text"
+        placeholder="Search by Name or Course Number"
+        value={searchTerm}
+        onChange={handleSearch}
+      />
       <table>
         <thead>
           <tr>
-            <th>Trimester</th>
-            <th>Course Number</th>
-            <th>Courses Name</th>
-            <th>Semester Credits</th>
-            <th>Total Clock Hours</th>
-            <th>Enroll</th>
+            <th
+              className="header-trimester"
+              onClick={() => handleSort("trimester")}
+            >
+              Trimester{" "}
+              {sortColumn === "trimester" &&
+                (sortDirection === "asc" ? "↑" : "↓")}
+            </th>
+            <th
+              className="header-number"
+              onClick={() => handleSort("courseNumber")}
+            >
+              Course Number{" "}
+              {sortColumn === "courseNumber" &&
+                (sortDirection === "asc" ? "↑" : "↓")}
+            </th>
+            <th
+              className="header-name"
+              onClick={() => handleSort("courseName")}
+            >
+              Course Name{" "}
+              {sortColumn === "courseName" &&
+                (sortDirection === "asc" ? "↑" : "↓")}
+            </th>
+            <th
+              className="header-credits"
+              onClick={() => handleSort("semesterCredits")}
+            >
+              Semester Credits{" "}
+              {sortColumn === "semesterCredits" &&
+                (sortDirection === "asc" ? "↑" : "↓")}
+            </th>
+            <th
+              className="header-hours"
+              onClick={() => handleSort("totalClockHours")}
+            >
+              Total Clock Hours{" "}
+              {sortColumn === "totalClockHours" &&
+                (sortDirection === "asc" ? "↑" : "↓")}
+            </th>
+            <th className="header">Enroll</th>
           </tr>
         </thead>
         <tbody>
-          {courses.map((course) => (
-            <tr key={course.Number}>
-              <td className="courseTimester">{course.trimester}</td>
-              <td className="courseNumber">{course.courseNumber}</td>
-              <td className="courseName">{course.courseName}</td>
-              <td className="courseCredits">{course.semesterCredits}</td>
-              <td className="courseHours">{course.totalClockHours}</td>
+          {sortedCourses.map((course) => (
+            <tr key={course.courseNumber}>
+              <td className="course-trimester">{course.trimester}</td>
+              <td className="course-number">{course.courseNumber}</td>
+              <td className="course-name">{course.courseName}</td>
+              <td className="course-credits">{course.semesterCredits}</td>
+              <td className="course-hours">{course.totalClockHours}</td>
               <td>
                 <button>Enroll</button>
               </td>
